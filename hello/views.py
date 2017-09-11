0357397c6541a650744c11fffdbff6e7100e7f61
aniket kasar <aniket kasar>
2017-09-12 03:09:47 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer:Dipankar Chavare
    
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   Pipfile
    	new file:   Pipfile.lock
    	new file:   Procfile
    	new file:   Procfile.windows
    	new file:   app.json
    	new file:   db.sqlite3
    	new file:   gettingstarted/__init__.py
    	new file:   gettingstarted/__init__.pyc
    	new file:   gettingstarted/heroku_test_runner.py
    	new file:   gettingstarted/settings.py
    	new file:   gettingstarted/settings.pyc
    	new file:   gettingstarted/static/humans.txt
    	new file:   gettingstarted/urls.py
    	new file:   gettingstarted/urls.pyc
    	new file:   gettingstarted/wsgi.py
    	new file:   gettingstarted/wsgi.pyc
    	new file:   hello/__init__.py
    	new file:   hello/__init__.pyc
    	new file:   hello/admin.py
    	new file:   hello/admin.pyc
    	new file:   hello/migrations/0001_initial.py
    	new file:   hello/migrations/0001_initial.pyc
    	new file:   hello/migrations/__init__.py
    	new file:   hello/migrations/__init__.pyc
    	new file:   hello/models.py
    	new file:   hello/models.pyc
    	new file:   hello/static/lang-logo.png
    	new file:   hello/templates/base.html
    	new file:   hello/templates/db.html
    	new file:   hello/templates/index.html
    	new file:   hello/tests.py
    	new file:   hello/views.py
    	new file:   hello/views.pyc
    	new file:   manage.py

---
+++ hello/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+import re
+
+import email
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
+
+
+def db(request):
+
+    # greeting = Greeting()
+    # greeting.save()
+
+    # greetings = Greeting.objects.all()
+    mailData={}
+    greetings=['hi','hello']
+    if request.method == 'POST':
+    	encodedMail=request.POST['mailHeader']
+    	encodedMail=encodedMail.encode('utf-8').strip()
+    	try:
+    		decodedMail = email.message_from_string(encodedMail)
+    		if decodedMail.keys():
+    			#Find to
+    			try:
+    				mailData['To']=decodedMail['Delivered-To']
+    			except:
+    				mailData['To']="NOT ABLE TO PARSE"
+
+
+    			#Find From
+    			try:
+    				mailData['From']=decodedMail['From']
+    			except:
+    				mailData['From']="NOT ABLE TO PARSE"
+
+
+    			#Find senders ip
+    			try:
+    				for item in decodedMail['Received-SPF'].split(';'):
+    					if 'client-ip' in item.lower():
+    						mailData['Sender_IP']=item.split('=')[-1]
+    				mailData['Sender_IP']=mailData['Sender_IP']
+    			except Exception as err:
+    				print err
+    				mailData['Sender_IP']="NOT ABLE TO PARSE"
+
+    			#Set of IPs
+    			try:
+    				mailData['Mail_Trace_Path']=[]
+    				for item in decodedMail.keys():
+    					if 'received' == item.lower():
+    						mailData['Mail_Trace_Path']=mailData['Mail_Trace_Path']+re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", str(decodedMail[item]))
+
+    				mailData['Mail_Trace_Path']=list(set(mailData['Mail_Trace_Path']))
+    				mailData['Mail_Trace_Path']=[mailData['From']]+list(reversed(mailData['Mail_Trace_Path']))+[mailData['To']]
+
+    			except Exception as err:
+    				print err
+    				mailData['Mail_Trace_Path']=[mailData['From']]+[mailData['To']]
+    				# mailData['Mail_Trace_Path']="NOT ABLE TO PARSE"
+
+
+    			#Content 
+    			try:
+    				mailData['Content']=''
+    				if decodedMail.is_multipart():
+    					for payload in decodedMail.get_payload():
+    						mailData['Content']=mailData['Content']+str(payload.get_payload())
+    				else:
+    					mailData['Content']=str(decodedMail.get_payload())
+					    
+
+    				
+
+    			except Exception as err:
+    				print err
+    				mailData['Content']="NOT ABLE TO PARSE"
+
+    			
+
+    		else:
+    			return render(request, 'db.html')
+    	except Exception as err:
+    		print err
+    		pass
+
+    
+    print mailData
+    return render(request, 'db.html',mailData)
+
